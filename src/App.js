83af0bc26b1e84c691841dfb01689087c2f21690
sagar nemkul <sagarnemkul7@gmail.com>
2023-06-20 15:25:33 +0545

    fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Login } from './login';
-import { Register } from './register';
+import { Login } from './Login';
+import { Register } from './Register';
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
